0da721a79c85f9caad498501d0f122f4a557a62e
thegergo02 <work@varigergo.xyz>
2022-07-15 09:41:48 +0200

    fix: zitadel

--- module.nix
+++ module.nix
@@ -18,6 +18,13 @@ in {
       type = types.path;
       description = "Path where CockroachDB should store it's data.";
     };
+    startScript = mkOption {
+      type = types.str;
+      description = "Script to start CockroachDB with.";
+      default = ''
+          ${cfg.package}/bin/cockroach start-single-node --insecure --http-addr :9090 --listen-addr=localhost --background
+      '';
+    }; 
   };
 
   config = mkIf cfg.enable {
@@ -25,9 +32,7 @@ in {
       description = "Starts CockroachDB.";
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
-        ExecStart = ''
-          ${cfg.package}/bin/cockroach start-single-node --insecure --http-addr :9090 --listen-addr=localhost
-          '';
+        ExecStart = cfg.startScript;
         WorkingDirectory = cfg.workingDirectory;
         Type = "simple";
       };
